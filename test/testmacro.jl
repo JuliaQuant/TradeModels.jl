c075c56d6c8f9c4866a6220eeeeab51ed4137700
milktrader <milktrader@gmail.com>
2013-12-12 18:37:42 -0500

    fixed testmacro

--- test/testmacro.jl
+++ test/testmacro.jl
@@ -1,14 +1,5 @@
 macro trademodels()
   println("")
-  reload(Pkg.dir("TradeModels/test/testmacro.jl"))
+  reload(Pkg.dir("TradeModels/test/runtests.jl"))
 end
 
-########### time trial wrapper #####################################
-
-function timetrial(f::Function, v::Any, n::Int)
-  p = Float64[]
-  for i in 1:n+1
-    push!(p, @elapsed f(v))
-  end
-  mean(p[2:end]) # toss out the first execution from the average
-end
