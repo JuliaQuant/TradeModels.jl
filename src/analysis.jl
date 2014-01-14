32a0f3a5e97b94becc2591fce4eb6f9084196cfa
milktrader <milktrader@gmail.com>
2014-01-13 21:18:41 -0500

    adding kwarg for now

--- src/analysis.jl
+++ src/analysis.jl
@@ -5,10 +5,18 @@ function annualizedreturn{T,V}(sa::Array{SeriesPair{T,V}, 1}, ndays::Int)
   exp(sum(diff(log(value(sa))))) ^ (252/ndays) - 1 
 end
 
+function annualizedreturn{T,V}(sa::Array{SeriesPair{T,V}, 1}; prices=false)
+  prices ?
+  exp(sum(diff(log(value(sa))))) ^ (252/size(sa,1)) - 1 :
+  exp(sum(sa)) - 1
+end
+
 ######## equity curve ########################
 
 # returns an array that depicts a curve
-function equity{T,V}(sa::Array{SeriesPair{T,V}, 1})
-  expm1(cumsum(diff(log(value(sa))))) + 1
+function equity{T,V}(sa::Array{SeriesPair{T,V}, 1}; prices=false)
+  prices ?
+  expm1(cumsum(diff(log(value(sa))))) + 1 :
+  expm1(cumsum(sa))
 end
 
