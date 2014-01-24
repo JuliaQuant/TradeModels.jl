ef1519eed46caeda38099d6bdb84dd04b92e9fd0
milktrader <milktrader@gmail.com>
2014-01-24 09:11:50 -0500

    initial changes, many things broken

--- src/indicator.jl
+++ src/indicator.jl
@@ -1,1 +1,16 @@
 # methods for indicator
+
+type TradeSignal
+
+  series::Array{SeriesPair,1}
+  indicator::Function
+  relationship::Function
+  otherseries::Array{SeriesPair,1}
+  whentrue::Array{Date{ISOCalendar}, 1}
+  
+TradeSignal() = (x=new(); x.whentrue = istrue(magic))
+                          
+
+#SelfReferential() = (x = new(); x.obj = x)
+end
+
