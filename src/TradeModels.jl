cfeaf1555f66ea4e11eba48d789534b0091e6b33
milktrader <milktrader@gmail.com>
2014-03-23 22:15:22 -0400

    added discretesignal

--- src/TradeModels.jl
+++ src/TradeModels.jl
@@ -7,17 +7,18 @@ using TimeSeries, MarketTechnicals
 export tradesignal, 
        annualizedreturn,
        equity,
-       previoustradeday,
-       continuereturns,
-       nothinghappenedreturn, 
-       printtoprint
+       discretesignal
+#        previoustradeday,
+#        continuereturns,
+#        nothinghappenedreturn, 
+#        printtoprint
 
 ################## include files #####################
 
 include("analysis.jl")
 # include("indicator.jl")
 # include("rule.jl")
-# include("signal.jl")
+include("signal.jl")
 # include("strategy.jl")
 # include("tasks.jl")
 # include("utilities.jl")
--- src/signal.jl
+++ src/signal.jl
@@ -1,14 +1,31 @@
+# convert bool to int and squish a continuous stream to unique, discrete occurances
+
+function discretesignal(ba::TimeArray{Bool,1})
+    iba = int(ba.values)
+    idx = [1]
+    for b in 2:length(ba)
+        if ba.values[b] !== ba.values[b-1]
+            push!(idx, b)
+        end
+    end
+    TimeArray(ba.timestamp[idx], iba[idx], ["signal"])
+end
+
+
+
+
+
 # methods for Signal
 
 # relationship between two SA || one SA and a value
 # returns an array of dates
-
-function tradesignal{T}(ta1::TimeArray{T,1}, ta2::TimeArray{T,1}, op::Function)
-    findwhen(op(ta1, ta2))
-end  
-
-function tradesignal{T}(ta::TimeArray{T,1}, val::Union(Float64, Int), op::Function)
-  idx = index(ta) 
-  val = op(values(ta), val)
-  index(istrue(SeriesTimeArray(idx, val)))
-end  
+#  
+#  function tradesignal{T}(ta1::TimeArray{T,1}, ta2::TimeArray{T,1}, op::Function)
+#      findwhen(op(ta1, ta2))
+#  end  
+#  
+#  function tradesignal{T}(ta::TimeArray{T,1}, val::Union(Float64, Int), op::Function)
+#    idx = index(ta) 
+#    val = op(values(ta), val)
+#    index(istrue(SeriesTimeArray(idx, val)))
+#  end  
