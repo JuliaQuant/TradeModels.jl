d11df08edbc5fdf9c2a3b43e0e1f249d931a81e7
milktrader <milktrader@gmail.com>
2014-05-28 21:41:28 -0400

    have standard error of multiple regressors working

--- src/dev/fitcl.jl
+++ src/dev/fitcl.jl
@@ -31,21 +31,22 @@ end
 ret = percentchange(cl, method="log")
 ret = flipud(ret.values)
 
-type AR
-    target
-    features   # design matrix
-    params
-    errors
-    se         # standard errors
-end
+# type AR
+#     target
+#     features   # design matrix
+#     params
+#     errors
+#     se         # standard errors
+# end
 
-function Base.show(ar::ARModel)
+function Base.show(ar::AR)
 #    println(io, @sprintf("%d", 4))
     println(io, "")
 end
 
 #function ar(v::Vector{Float64}, p::Int)
 function ar(ta::TimeArray{Float64,1}, p::Int)
+
     pc = percentchange(ta, method="log")
     v  = flipud(pc.values)
     Xs = ones(length(v)-p, p+1)
@@ -54,10 +55,10 @@ function ar(ta::TimeArray{Float64,1}, p::Int)
     end
     y = v[p+1:end]
 
-    # sigs = findall(abs(autocor(v)) .> .05) # find rows that pass .05 confidence
-    # sig  = sigs[sigs.<11] # don't care about lags more than 10
+    sigs = findall(abs(autocor(v)) .> .05) # find rows that pass .05 confidence
+    sig  = sigs[sigs.<11] # don't care about lags more than 10
 
-    sig = 1:p+1 #canonical method to include all features up to and including the last one that shows serial correlation
+    #sig = 1:p+1 #canonical method to include all features up to and including the last one that shows serial correlation
 
     X = Xs[:,sig] # the first column remains 1s, some put this as the last
     # Normal Equations
@@ -68,9 +69,10 @@ function ar(ta::TimeArray{Float64,1}, p::Int)
     beta    = inv(R)Q'y
     epsilon = y-X*beta
     mse     = mean(epsilon.^2)
-    stderr  = diag(sqrt(mse))
+    ∑       = mse * inv(X'X)
+    stderr  = sqrt(diag(∑)) # diag(sqrtm(∑))
 
     AR(y,X,beta,epsilon,stderr)
 end
 
-myfit = ar(c["Settle"],6)
+#myfit = ar(c["Settle"],6)
